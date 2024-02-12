138e914c678d43b14598d9b0420fac693b16ba43
sunforest <leesunforest@gmail.com>
2024-02-12 18:44:57 +0900

    hello
    hello

---
+++ HelloPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var greeting = "Hello, Swift!"
+
+print(greeting)
+print(greeting)
+
+UIImage(systemName: "star")
+
+var sum = 0
+for i in 1...10 {
+	sum += i
+}
+print(sum)
---
+++ HelloSwift.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+import MapKit
+
+var greeting = "Hello, playground"
+
+print(greeting)
+dump(greeting)
